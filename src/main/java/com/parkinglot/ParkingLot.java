8770dfd4114e72286e61ee5a36b1a928fca20ec2
de Mesa, Adrian Kyle <adrian.kyle.de.mesa@oocl.com>
2022-09-02 15:13:59 +0800

    feat: Test should_return_right_car_when_fetch_twice_given_parking_lot_with_two_parked_cars_and_two_ticket completed add function such as fetch and also add Map

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,13 +1,23 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
     private Car car;
+
+    private Map<ParkingTicket, Car> parkedPosition;
+    public ParkingLot(){
+        this.parkedPosition = new HashMap<>();
+    }
+
     public ParkingTicket park(Car car) {
-        this.car = car;
-        return new ParkingTicket();
+        ParkingTicket ticket = new ParkingTicket();
+        this.parkedPosition.put(ticket, car);
+        return ticket;
     }
 
     public Car fetch(ParkingTicket ticket) {
-    return car;
+    return this.parkedPosition.get(ticket);
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -33,5 +33,26 @@ public class ParkingLotTest {
     assertEquals(car, fetchedCar);
   }
 
+  @Test
+  void should_return_righ_car_when_fetch_twice_given_parking_lot_with_two_parked_cars_and_two_ticket(){
+    //given
+    ParkingLot parkingLot = new ParkingLot();
+    Car carA = new Car();
+    Car carB = new Car();
+    ParkingTicket ticketA = parkingLot.park(carA);
+    ParkingTicket ticketB = parkingLot.park(carB);
+
+    //When
+
+    Car fetchedCarWithTicketA = parkingLot.fetch(ticketA);
+    Car fetchedCarWithTicketB = parkingLot.fetch(ticketB);
+
+    //then
+    assertEquals(carA, fetchedCarWithTicketA);
+    assertEquals(carB, fetchedCarWithTicketB);
+  }
+
+
+
 
 }
